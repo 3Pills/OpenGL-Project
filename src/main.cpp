322d315350f98eecbcb586fb64a12a94366c800e
Stephen Koren <stephenkoren7@gmail.com>
2015-02-05 15:56:45 +1100

    Added a grid and some shapes.

--- src/main.cpp
+++ src/main.cpp
@@ -36,9 +36,43 @@ int main() {
 	printf("OpenGL V%d.%d successfully loaded.", major_version, minor_version);
 
 	glClearColor(0.3f, 0.3f, 0.3f, 1.0f);
+	glEnable(GL_DEPTH_TEST);
 
+	Gizmos::create();
+
+	mat4 view = glm::lookAt(vec3(10, 10, 10), vec3(0, 0, 0), vec3(0, 1, 0));
+	mat4 proj = glm::perspective(glm::radians(60.0f), 1280.0f / 720.0f, 0.1f, 1000.0f);
+
+	vec3 pos = vec3(10, 10, 10);
+	float timer = 0.0f;
+
+	glfwSetTime(0.0);
 	while (!glfwWindowShouldClose(window)){
+		float dt = (float)glfwGetTime();
+		glfwSetTime(0.0);
+		timer += dt;
+
 		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+		Gizmos::clear();
+		Gizmos::addTransform( mat4(1), 10);
+
+		view = glm::lookAt(pos, vec3(0, 0, 0), vec3(0, 1, 0));
+		//pos.x = sinf(timer) * 10;
+		//pos.z = cosf(timer) * 10;
+
+		vec4 white(1);
+		vec4 black(0, 0, 0, 1);
+		vec4 red(1, 0, 0, 1);
+		vec4 green(0, 1, 0, 1);
+		vec4 blue(0, 0, 1, 1);
+
+		for (int i = 0; i <= 20; ++i){
+			Gizmos::addLine(vec3(-10 + i, 0, -10), vec3(-10 + i, 0, 10), i == 10 ? white : black);
+			Gizmos::addLine(vec3(-10, 0, -10 + i), vec3(10, 0, -10 + i), i == 10 ? white : black);
+		}
+		Gizmos::addTri(vec3(0, 0, 0), vec3(0, 0, 5), vec3(0, 5, 0), green);
+		Gizmos::addAABBFilled(vec3(0, 4, -5), vec3(1,1,1), blue);
+		Gizmos::draw(proj, view);
 
 		glfwSwapBuffers(window);
 		glfwPollEvents();
