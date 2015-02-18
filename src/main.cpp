92fc6bf8417e2466fc14ddb8013389de9c4e0eb6
Stephen Koren <stephenkoren7@gmail.com>
2015-02-19 09:49:33 +1100

    Fixed strange commit merging things

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,7 @@
-<<<<<<< HEAD
 #include "AdvancedTextures.h"
 
 int main() {
 	AdvancedTextures App;
-=======
-#include "Textures.h"
-
-int main() {
-	Textures App;
->>>>>>> parent of 035ea57... Began work on model lighting.
 	App.startup();
 
 	while (App.update()){
